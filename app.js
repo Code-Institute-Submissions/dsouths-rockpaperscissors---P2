8a7f795264d6e488d086ec21253c77338201bb3c
dsouths <dsouths@hotmail.com>
2022-09-18 12:39:32 +0000

    defined necessary variables in js needed for the game

---
+++ app.js
@@ -0,0 +1,10 @@
+const userScore = 0;
+const computerScore = 0;
+const userScore_span = document.getElementById('user-score');
+const computerScore_span = document.getElementById('computer-score');
+const scoreBoard_div = document.querySelector('.score-board');
+const gameResult_div = document.querySelector('.game-result');  
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
+
