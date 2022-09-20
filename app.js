e560c834df0c5c9655849ca905dafbc2e30859da
dsouths <dsouths@hotmail.com>
2022-09-20 13:53:44 +0000

    working on win function

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById('user-score');
 const computerScore_span = document.getElementById('computer-score');
 const scoreBoard_div = document.querySelector('.score-board');
-const gameResult_div = document.querySelector('.game-result');  
+const gameResult_p = document.querySelector('.game-result > p');  
 const rock = document.getElementById('rock');
 const paper = document.getElementById('paper');
 const scissors = document.getElementById('scissors');
 
-function getComputerChoice() {
+function getComputerChoice() { 
     const choices = ['rock', 'paper', 'scissors'];
     const randomNumber = (Math.floor(Math.random() * 3));
     // returns element rather than random number//
     return choices[randomNumber];
+}
+
+function win(userChoice, computerChoice) {
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    console.log(user);
+    console.log(computer);
+}
+
+function lose() {
+    computerScore++;
+    computerScore_span.innerHTML = computerScore;
+    
+}
+
+function draw() {
+    console.log("draw")
 }
 
 function game(userChoice) { 
@@ -21,33 +38,23 @@ function game(userChoice) {
         case "rockscissors":
         case "paperrock":
         case "scissorspaper":
-            win();
+            win(userChoice, computerChoice);
             break;
         case "rockpaper":
         case "paperscissors":
         case "scissorsrock":
-            lose();
+            lose(userChoice, computerChoice);
             break;   
         case "rockrock":
         case "paperpaper":
         case "scissorsscissors":
-            draw();
+            draw(userChoice, computerChoice);
             break;      
     }
        
 }
 
-function win() {
-    console.log("win")
-}
 
-function lose() {
-    console.log("lose")
-}
-
-function draw() {
-    console.log("draw")
-}
 
 function main() {
     rock.addEventListener('click', function () {
